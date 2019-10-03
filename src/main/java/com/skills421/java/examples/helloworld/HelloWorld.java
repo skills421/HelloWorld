390e50e3bcd2d3d4980c20ee36511acaadcacc37
John Dunning <john.dunning@skills421.com>
2019-10-03 22:32:23 +0100

    first commit

---
+++ src/main/java/com/skills421/java/examples/helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.skills421.java.examples.helloworld;
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
